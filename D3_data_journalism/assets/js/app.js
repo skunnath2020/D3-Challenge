cc887eae159f8216c1bc1ab82ba99629c564c227
skunnath2020 <sskunnath@gmail.com>
2021-03-05 16:03:24 -0500

    wip

--- D3_data_journalism/assets/js/app.js
+++ D3_data_journalism/assets/js/app.js
@@ -1,4 +1,52 @@
-// @TODO: YOUR CODE HERE!
+//setup SVG area
+//=======================
+var svgWidth = 960;
+var svgHeight = 500;
+var margin = {
+    top : 20,
+    right: 40 , 
+    bottom:60,
+    left: 50
+
+};
+var width = svgWidth - margin.left - margin.right;
+var height = svgHeight - margin.top - margin.bottom;
+
+// step 2: setup SVG wrapper
+var svg = d3.select("body")
+        .append("svg")
+        .attr("width", svgWidth)
+        .attr("height", svgHeight);
+var charGroup = svg.append("g")
+    .attr("transform", `translate(${margin.left}), ${margin.right})`)
+
+//step3: data binding
 d3.csv("/data/data.csv").then(function(data){
-    console.log(data);
+    data.forEach(function(data){
+        var state=data.abbr;
+        var poverty=data.povertyMoe;
+        
+    })
+
+    //step 4 : create the scale from the chart
+    var xLinearScale = d3.scalelinear()
+            .domain(d3.extend(data, d => d.abbr)) 
+            .range([0, width]);
+    var yLinearScale = d3.scaleLinear()
+            .domain([0, d3.max(data, d => d.povertyMoe)])
+            .range([height, 0]);
 })
+
+    //step -5 Create the axes
+    var bottomAxis = d3.axisBottom(xLinearScale);
+    var leftAxis = d3.axisLeft(yLinearScale);
+    //step 6 append the axes to the chartGroup
+    chartGroup.append("g")
+    .attr("transform", `translate(0, ${height})`)
+    .call(bottomAxis);
+
+    chartGroup.append("g").call(leftAxis);
+
+//create a scatter plot between two of the data variables such as `Healthcare vs. Poverty` or `Smokers vs. Age`.
+
+
